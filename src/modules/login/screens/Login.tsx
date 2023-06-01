fe2e6c691a1a3f7947971d4bc8920f8e37c4687a
heenrYnog <henryfurlan2002@gmail.com>
2023-06-01 18:18:01 -0300

    botao global criado

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,14 +1,20 @@
 import { Text, View } from 'react-native';
 
+import Button from '../../../shared/components/button/Button';
 import Input from '../../../shared/components/input/Input';
 import { ContainerLogin } from '../styles/login.style';
 
 const Login = () => {
+  const handleOnPress = () => {
+    console.log('clicou');
+  };
+
   return (
     <View>
       <ContainerLogin>
         <Text>Zap</Text>
         <Input />
+        <Button margin="16px" title="ENTRAR" onPress={handleOnPress} />
       </ContainerLogin>
     </View>
   );
