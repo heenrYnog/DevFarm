f5ddfc4cd2e96872ebe23bdd697cd7020876918e
heenrYnog <henryfurlan2002@gmail.com>
2023-06-01 17:05:34 -0300

    styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { SafeAreaView, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const TextNew = styled.Text`
+  font-size: 24px;
+  color: pink;
+`;
 
 const App = () => {
   return (
     <SafeAreaView>
       <Text>Testando</Text>
+      <TextNew>NovoText</TextNew>
     </SafeAreaView>
   );
 };
