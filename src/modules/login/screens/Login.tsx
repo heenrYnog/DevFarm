13fa57da201c9f5f6cd57351cf967cf11ad56b91
heenrYnog <henryfurlan2002@gmail.com>
2023-06-01 20:09:58 -0300

    fonts inseridas

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import Button from '../../../shared/components/button/Button';
 import Input from '../../../shared/components/input/Input';
@@ -12,7 +12,6 @@ const Login = () => {
   return (
     <View>
       <ContainerLogin>
-        <Text>Zap</Text>
         <Input />
         <Button margin="16px" title="ENTRAR" onPress={handleOnPress} />
       </ContainerLogin>
---
+++ src/shared/components/text/text.style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+interface ContainerTextProps {
+  color?: string;
+  fontSize: string;
+}
+
+export const ContainerText = styled.Text<ContainerTextProps>`
+  ${(props) => (props.color ? `color: ${props.color}` : '')}
+  font-family: Poppins-Bold;
+  font-size: ${(props) => props.fontSize};
+`;
