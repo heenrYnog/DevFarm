c86918b33e5b3db662ecbe0632ad773ddff32925
heenrYnog <henryfurlan2002@gmail.com>
2023-06-01 20:49:54 -0300

    tipos de textos

--- src/shared/components/button/Button.tsx
+++ src/shared/components/button/Button.tsx
@@ -13,7 +13,7 @@ interface ButtonProps extends TouchableOpacityProps {
 const Button = ({ title, margin, ...props }: ButtonProps) => {
   return (
     <ButtonContainer margin={margin} {...props}>
-      <Text type={textTypes.SUB_TITLE_BOLD} color={theme.colors.neutralTheme.white}>
+      <Text type={textTypes.BUTTON_BOLD} color={theme.colors.neutralTheme.white}>
         {title}
       </Text>
     </ButtonContainer>
