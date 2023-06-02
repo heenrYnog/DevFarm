00ee77fc8f2294205d7600c538efd8c95e2dc7d1
heenrYnog <henryfurlan2002@gmail.com>
2023-06-02 00:29:57 -0300

    icones instalado

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,6 +1,7 @@
 import { View } from 'react-native';
 
 import Button from '../../../shared/components/button/Button';
+import { Icon } from '../../../shared/components/icon/Icon';
 import Input from '../../../shared/components/input/Input';
 import { theme } from '../../../shared/themes/theme';
 import { ContainerLogin } from '../styles/login.style';
@@ -13,6 +14,7 @@ const Login = () => {
   return (
     <View>
       <ContainerLogin>
+        <Icon name="home3" />
         <Input placeholder="Digite seu e-mail.." title="E-mail:" />
         <Button
           type={theme.buttons.buttonsTheme.primary}
