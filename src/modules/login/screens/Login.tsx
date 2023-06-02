6c9a4267196ef7197dfe5090b9da11ea265b77d5
heenrYnog <henryfurlan2002@gmail.com>
2023-06-02 04:41:57 -0300

    Imagem login

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -3,7 +3,7 @@ import { View } from 'react-native';
 import Button from '../../../shared/components/button/Button';
 import Input from '../../../shared/components/input/Input';
 import { theme } from '../../../shared/themes/theme';
-import { ContainerLogin } from '../styles/login.style';
+import { ContainerLogin, Imagelogo } from '../styles/login.style';
 
 const Login = () => {
   const handleOnPress = () => {
@@ -13,6 +13,7 @@ const Login = () => {
   return (
     <View>
       <ContainerLogin>
+        <Imagelogo resizeMode="contain" source={require('../../../assets/images/logo.png')} />
         <Input margin="0px 0px 8px 0px" placeholder="Digite seu e-mail.." title="E-mail:" />
         <Input secureTextEntry placeholder="Digite sua senha.." title="Senha:" />
         <Button
