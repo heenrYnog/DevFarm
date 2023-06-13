813d38cec36971fe305bc680a3cebd9d9ef50271
heenrYnog <henryfurlan2002@gmail.com>
2023-06-13 20:59:27 -0300

    await

--- src/shared/hooks/useRequest.ts
+++ src/shared/hooks/useRequest.ts
@@ -64,7 +64,7 @@ export const useRequest = () => {
 
   const authRequest = async (body: RequestLogin) => {
     setLoading(true);
-    await connectionAPIPost<ReturnLogin>('http://192.168.15.10:8080/auth', body)
+    await connectionAPIPost<ReturnLogin>('http://localhost:8080/auth', body)
       .then((result) => {
         setAuthorizationToken(result.accessToken);
         setUser(result.user);
